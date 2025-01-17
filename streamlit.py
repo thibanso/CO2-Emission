6db1a2b2d1d9b5223a049d02cb0ecdec3e61de8e
Thibault A. El Mansouri <thibault.elmansouri@gmail.com>
2025-01-17 15:38:08 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -83,7 +83,7 @@ if page == pages[0]: #Contexte
     st.title("Prédictions d'émission de CO₂")
     st.divider()
 
-    st.markdown(
+st.markdown(
     """
     <style>
     .center-image {
@@ -96,9 +96,10 @@ if page == pages[0]: #Contexte
     <div class="center-image">
         <img src="image/image_intro.webp" width="400" />
     </div>
-    """,
+    """, 
     unsafe_allow_html=True
 )
+
 
     st.write("### Introduction")
     st.write("""Le dioxyde de carbone, communément appelé CO2, est un composant vital de notre atmosphère qui
